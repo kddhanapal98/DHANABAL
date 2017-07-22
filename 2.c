6c96a421101be91a1890292a62609c23374878d4
kddhanapal98 <30342485+kddhanapal98@users.noreply.github.com>
2017-07-22 16:54:25 +0530

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+double number;
+printf("enter a number:");
+scanf("%if",&number);
+if(number<=0.0)
+{
+if(number==0.0)
+printf"you entered 0.");
+else
+printf(you entered a negative number:");
+}
+else
+printf("you entered a positive number:");
+return0;
+}
