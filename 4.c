919f9ba4b5e7f712ac7dfa8a03b49b6887fbe08c
kddhanapal98 <30342485+kddhanapal98@users.noreply.github.com>
2017-08-11 16:56:30 +0530

    Update 4.c

--- 4.c
+++ 4.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 int main()
 {
-    char c;
+    char d;
     int isLowercaseVowel, isUppercaseVowel;
 
     printf("Enter an alphabet: ");
-    scanf("%c",&c);
+    scanf("%d",&d);
 
-    // evaluates to 1 (true) if c is a lowercase vowel
-    isLowercaseVowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+    // evaluates to 1 (true) if d is a lowercase vowel
+    isLowercaseVowel = (d == 'a' || d == 'e' || d == 'i' || d == 'o' || d == 'u');
 
-    // evaluates to 1 (true) if c is an uppercase vowel
-    isUppercaseVowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
+    // evaluates to 1 (true) if d is an uppercase vowel
+    isUppercaseVowel = (d == 'A' || d == 'E' || d == 'I' || d == 'O' || d == 'U');
 
     // evaluates to 1 (true) if either isLowercaseVowel or isUppercaseVowel is true
     if (isLowercaseVowel || isUppercaseVowel)
-        printf("%c is a vowel.", c);
+        printf("%d is a vowel.", d);
     else
-        printf("%c is a consonant.", c);
+        printf("%c is a consonant.", d);
     return 0;
 }
