0c32dda0ff2783e2e227183b47cd401cd55a675c
kddhanapal98 <30342485+kddhanapal98@users.noreply.github.com>
2017-08-12 16:27:06 +0530

    Update 14.c

--- 14.c
+++ 14.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 int main()
 {
-    int n, i, flag = 0;
+    int m, i, flag = 0;
 
     printf("Enter a positive integer: ");
-    scanf("%d",&n);
+    scanf("%d",&m);
 
-    for(i=2; i<=n/2; ++i)
+    for(i=2; i<=m/2; ++i)
     {
         // condition for nonprime number
-        if(n%i==0)
+        if(m%i==0)
         {
             flag=1;
             break;
@@ -17,9 +17,9 @@ int main()
     }
 
     if (flag==0)
-        printf("%d is a prime number.",n);
+        printf("%d is a prime number.",m);
     else
-        printf("%d is not a prime number.",n);
+        printf("%d is not a prime number.",m);
     
     return 0;
 }
